dd03b5b697f41e04d562b5608e5d52e4e25114ae
tuserl <153376685+tuserl@users.noreply.github.com>
2026-06-28 16:46:32 +0700

    updated e2e and pvc package to 1.3.0

--- e2e-tests/commission/runtest.ps1
+++ e2e-tests/commission/runtest.ps1
@@ -1,2 +1,2 @@
-npx playwright test -g "TC01"
-npx playwright test commission-form.spec.js --headed --reporter=list,html
+#npx playwright test -g "TC01"
+npx playwright test commission-form.spec.js --headed --reporter=list,html --workers=2
